cd1f6c03f79511f3675f7230381f0e071708f45e
UdyneO2 <dynemuhamad@gmail.com>
2026-06-22 23:58:35 +0700

    update

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ or if you have one running,
 ```tmux a -t <session number>```
 
 ## Pull and Run this script
-```$ curl -sf https://raw.githubusercontent.com/sounddrill31/android_build_scripts/lineage-oxygen/build.sh | bash```
+```$ curl -sf https://raw.githubusercontent.com/UdyneO2/android_build_scripts/refs/heads/lineage-oxygen/build.sh | bash```
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 #Credit to Meghthedev for the initial script 
 
@@ -8,6 +7,7 @@ export PROJECTID="85"
 export REPO_INIT="repo init -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs --depth=1"
 export BUILD_DIFFERENT_ROM="$REPO_INIT" # Change this if you'd like to build something else
 
+craveclone(){
 # Destroy Old Clones
 if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) || [ "${DCDEVSPACE}" == "1" ]; then   
    crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
@@ -25,19 +25,23 @@ else
    echo "Running $REPO_INIT"
    $REPO_INIT
 fi
+}
+
+#craveclone
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
+cd $PROJECTFOLDER
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
 # Init Manifest
 $BUILD_DIFFERENT_ROM && \
 
 # clone source
-
+rm -rf device/oppo/A37 vendor/oppo/A37 kernel/oppo/msm8939
 git clone https://github.com/udyneos-prjkt/android_device_oppo_A37 -b lineage-18.1 device/oppo/A37
-git clone https://github.com/UdyneO2/rb-vendor_oppo vendor/oppo/A37
-git clone https://github.com/UdyneO2/kernel_oppo_A37-old kernel/oppo/msm8939
+git clone https://github.com/UdyneO2/rb-vendor_oppo vendor/oppo
+git clone https://github.com/UdyneO2/kernel_oppo_A37-old kernel/oppo/msm8939 --depth=1
 
  # Sync the repositories
  /opt/crave/resync.sh && \ 
@@ -46,15 +50,16 @@ git clone https://github.com/UdyneO2/kernel_oppo_A37-old kernel/oppo/msm8939
 source build/envsetup.sh && \
 
 # Build the ROM
-brunch A37"
+lunch lineage_A37-userdebug
+mka bacon"
 
-cd ..
+# cd ..
 
-# Clean up
-if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') || [ "${DCDEVSPACE}" == "1" ]; then
-  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
-else  
-  rm -rf $PROJECTFOLDER || true
-fi
+# # Clean up
+# if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') || [ "${DCDEVSPACE}" == "1" ]; then
+  # crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+# else  
+  # rm -rf $PROJECTFOLDER || true
+# fi
 # Upload zips to Telegram
 /opt/crave/telegram/upload.sh
